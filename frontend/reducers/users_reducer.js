f2fa8fdb6a7b6653e8eb8bfcb55c8908966da263
christopherclow <christopher.ck.low@gmail.com>
2021-04-05 21:07:51 -0700

    Entities Reducer Updated

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_USERS } from '../actions/user_actions';
+import { RECEIVE_BUSINESS } from '../actions/business_actions';
+
+const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
+        default:
+            return state;
+    }
+}
+
+export default usersReducer;
