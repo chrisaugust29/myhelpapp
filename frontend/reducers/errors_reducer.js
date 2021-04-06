d6e53112ff5c256d22729c4529a1c7c5e4ce0570
christopherclow <christopher.ck.low@gmail.com>
2021-04-05 21:46:05 -0700

    Root Reducer and error reducers

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import sessionErrorsReducer from './session_errors_reducer';
+import reviewErrorsReducer from "./reviews_errors_reducer";
+
+const errorsReducer = combineReducers({
+    session: sessionErrorsReducer,
+    review: reviewErrorsReducer
+});
+
+export default errorsReducer;
---
+++ frontend/reducers/reviews_errors_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_ERRORS, RECEIVE_REVIEW, } from "../actions/review_actions";
+
+export default (state = [], action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_REVIEW:
+            return [];
+        case RECEIVE_ERRORS:
+            return action.errors;
+ 
+        default:
+            return state;
+    }
+};
