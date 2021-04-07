a34215ccca0dad526a255cf36c5e9443606fc678
christopherclow <christopher.ck.low@gmail.com>
2021-04-06 22:34:04 -0700

    Auth Routes Completed

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import { Link, Route, Redirect, Switch,} from 'react-router-dom';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from "./session_form/login_form_container";
 import DirectorContainter from "./director/director_container";
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div>
@@ -10,8 +11,8 @@ const App = () => (
             <h1>Help App</h1>
         </Link>
      < Switch>
-        <Route path="/signup" component={SignupFormContainer} />
-        <Route path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
         <Route exact path="/" component={DirectorContainter} />
         <Route render={() => <Redirect to={{ pathname: "/" }} />} />
     </Switch>
