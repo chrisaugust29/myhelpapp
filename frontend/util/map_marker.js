61aa44d626209c89ff92ff846f3e8471a218ce33
christopherclow <christopher.ck.low@gmail.com>
2021-06-04 12:24:02 -0700

    map marker fix

--- frontend/util/map_marker.js
+++ frontend/util/map_marker.js
@@ -21,11 +21,11 @@ class MapMarker {
     const position = new google.maps.LatLng(business.lat, business.lng);
     const marker = new google.maps.Marker({
       position,
-    //   label: {
-    //     text: this.markerLabel.toString(),
-    //     color: "#ffffff",
-    //     fontWeight: "bold",
-    //   },
+      label: {
+        text: this.markerLabel.toString(),
+        color: "#ffffff",
+        fontWeight: "bold",
+      },
       map: this.map,
       businessId: business.id,
     });
