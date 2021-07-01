c8ec1dadc1dd7a4b3f38bb72c3d6cd1642440289
christopherclow <christopher.ck.low@gmail.com>
2021-07-01 14:36:49 -0700

    help

--- frontend/components/business/business_index.jsx
+++ frontend/components/business/business_index.jsx
@@ -14,6 +14,7 @@ class BusinessIndex extends React.Component {
 
     componentDidMount() {
         // debugger
+        window.scrollTo(0, 0);
         this.props.fetchBusinesses();
     }
     render() {
