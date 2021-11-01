faef57efb7eddb5c03475070d0199af4b5cb70b5
christopherclow <christopher.ck.low@gmail.com>
2021-11-01 11:15:32 -0700

    help

--- frontend/components/header/header_container.js
+++ frontend/components/header/header_container.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import { logout, errorClear } from '../../actions/session_actions';
 import Header from './header';
 
+
 const mapStateToProps = (state) => {
     return {
         currentUser: state.session.id,
